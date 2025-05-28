46126ba88a4772d5b617a52e7d336f16ed7ccd80
Lio <liobensignor@gmail.com>
2025-05-28 19:46:47 -0300

    Ejercicio 09

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header title="Todo App" />
+      {/* TodoApp */}
+      {/* TodoList */}
+      {/* TodoItem */}
+      {/* Footer */}
+    </>
+  )
+}
+
+export default App
